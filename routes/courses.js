6ef8be8e42e27646f21b481e8df4120fe877f14f
unknown <olena.denysova88@gmail.com>
2020-11-22 17:23:54 +0100

    delete course

--- routes/courses.js
+++ routes/courses.js
@@ -27,8 +27,6 @@ router.get('/:id/edit', async (req, res) => {
   if (!req.query.allow) {
     return res.redirect('/')
   }
-
-  //const course = await Course.findById({_id: req.params.id}).lean();
   const course = await Course.findById(req.params.id).lean()
   res.render('course-edit', {
     title: `Edit ${course.title}`,
@@ -43,5 +41,15 @@ router.post('/edit', async (req, res) => {
   await Course.findByIdAndUpdate(id, req.body).lean()
   res.redirect('/courses')
 })
+
+ //delete course
+  router.post('/remove', async (req, res) => {
+    try {
+      await Course.deleteOne({_id: req.body.id}).lean()
+      res.redirect('/courses')
+    } catch(err) {
+      console.log(err)
+    } 
+  })
 
 module.exports = router
