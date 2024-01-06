ad86a8a7ba190615cb4e9c01eb6cd1d9c3b7b5aa
unknown <olena.denysova88@gmail.com>
2024-01-06 17:51:02 +0100

    connect to db before port

--- index.js
+++ index.js
@@ -1,120 +1,132 @@
-const express = require('express')
-const path = require('path')
-const csrf = require('csurf')
-const flash = require('connect-flash')
-const mongoose = require('mongoose')
-const helmet = require('helmet')
-const compression = require('compression')
-const exphbs = require('express-handlebars')
-const Handlebars = require('handlebars')
-const {allowInsecurePrototypeAccess} = require('@handlebars/allow-prototype-access')
-const session = require('express-session')
-const MongoStore = require('connect-mongodb-session')(session)
+const express = require("express");
+const path = require("path");
+const csrf = require("csurf");
+const flash = require("connect-flash");
+const mongoose = require("mongoose");
+const helmet = require("helmet");
+const compression = require("compression");
+const exphbs = require("express-handlebars");
+const Handlebars = require("handlebars");
+const {
+    allowInsecurePrototypeAccess,
+} = require("@handlebars/allow-prototype-access");
+const session = require("express-session");
+const MongoStore = require("connect-mongodb-session")(session);
 
-const keys = require('./keys')
-const PORT = process.env.PORT || 3000
+const keys = require("./keys");
+const PORT = process.env.PORT || 3000;
 
-const homeRoutes = require('./routes/home')
-const addRoutes = require('./routes/add')
-const coursesRoutes = require('./routes/courses')
-const cartRoutes = require('./routes/cart')
-const ordersRoutes = require('./routes/orders')
-const authRoutes = require('./routes/auth')
-const profileRoutes = require('./routes/profile')
+const homeRoutes = require("./routes/home");
+const addRoutes = require("./routes/add");
+const coursesRoutes = require("./routes/courses");
+const cartRoutes = require("./routes/cart");
+const ordersRoutes = require("./routes/orders");
+const authRoutes = require("./routes/auth");
+const profileRoutes = require("./routes/profile");
 
-const varMiddleware = require('./middleware/variables')
-const userMiddleware = require('./middleware/user')
-const errorMiddleware = require('./middleware/error')
-const fileMiddleware = require('./middleware/file')
+const varMiddleware = require("./middleware/variables");
+const userMiddleware = require("./middleware/user");
+const errorMiddleware = require("./middleware/error");
+const fileMiddleware = require("./middleware/file");
 
-const app = express()
+const app = express();
 
 const hbs = exphbs.create({
-  defaultLayout: 'main', 
-  extname: 'hbs',
-  handlebars: allowInsecurePrototypeAccess(Handlebars),
-  helpers: require('./utils/hbs-helpers')
-})
+    defaultLayout: "main",
+    extname: "hbs",
+    handlebars: allowInsecurePrototypeAccess(Handlebars),
+    helpers: require("./utils/hbs-helpers"),
+});
 
 const store = new MongoStore({
-  collection: 'sessions',
-  uri: keys.MONGODB_URI
-})
+    collection: "sessions",
+    uri: keys.MONGODB_URI,
+});
 
-app.engine('hbs', hbs.engine)
-app.set('view engine', 'hbs')
-app.set('views', 'views')
+app.engine("hbs", hbs.engine);
+app.set("view engine", "hbs");
+app.set("views", "views");
 
 //MIDDLEWARE
 //to use folder static, address to files in it "/<name>",or like files in root without "/<name>""
-app.use(express.static(path.join(__dirname, 'public')))
-app.use('public/pictures', express.static(path.join(__dirname, 'pictures')))
-app.use('/images', express.static(path.join(__dirname, 'images')))
-
+app.use(express.static(path.join(__dirname, "public")));
+app.use("public/pictures", express.static(path.join(__dirname, "pictures")));
+app.use("/images", express.static(path.join(__dirname, "images")));
 
 //to use req.body
-app.use(express.urlencoded({extended: true}))
+app.use(express.urlencoded({ extended: true }));
 
 //auth session
-app.use(session({
-  secret: keys.SESSION_SECRET,
-  resave: false,
-  saveUninitialized: false,
-  store
-}))
+app.use(
+    session({
+        secret: keys.SESSION_SECRET,
+        resave: false,
+        saveUninitialized: false,
+        store,
+    })
+);
 
 //to download file for avatar
-app.use(fileMiddleware.single('avatar'))
+app.use(fileMiddleware.single("avatar"));
 
 //for CSRF token creation and validation. This middleware adds a req.csrfToken() function to make a token which should be added to requests which mutate state, within a hidden form field, query-string etc. This token is validated against the visitor's session or csrf cookie.
-app.use(csrf())
+app.use(csrf());
 
 //for error messages
-app.use(flash())
+app.use(flash());
 
 //for security, manage headers
-app.use(helmet({
-  contentSecurityPolicy: false
-}))
+app.use(
+    helmet({
+        contentSecurityPolicy: false,
+    })
+);
 
-//compression 
-app.use(compression())
+//compression
+app.use(compression());
 
 //my middleware
-app.use(varMiddleware)
-app.use(userMiddleware)
+app.use(varMiddleware);
+app.use(userMiddleware);
 
-//ROUTES
-app.use('/', homeRoutes)
-app.use('/add', addRoutes)
-app.use('/courses', coursesRoutes)
-app.use('/cart', cartRoutes)
-app.use('/orders', ordersRoutes)
-app.use('/auth', authRoutes)
-app.use('/profile', profileRoutes)
-
-//error 404 handler, after all routers
-app.use(errorMiddleware)
-
-//connection to DB and port
-async function start() {
-  try {
-    await mongoose.connect(keys.MONGODB_URI, {
-      useUnifiedTopology: true,
-      useNewUrlParser: true,
-      useFindAndModify: false
-    })
-    console.log(`Connected to DB successfully`)
-
-    app.listen(PORT, () => {
-      console.log(`Server is running on port ${PORT}`)
-    })
-  } catch (err) {
-    console.log(err)
-  }
+//connection to DB
+async function connectDB() {
+    try {
+        await mongoose.connect(keys.MONGODB_URI, {
+            useUnifiedTopology: true,
+            useNewUrlParser: true,
+            useFindAndModify: false,
+        });
+        console.log(`Connected to DB successfully`);
+    } catch (err) {
+        console.log(err);
+    }
 }
 
-start()
+//ROUTES
+app.use("/", homeRoutes);
+app.use("/add", addRoutes);
+app.use("/courses", coursesRoutes);
+app.use("/cart", cartRoutes);
+app.use("/orders", ordersRoutes);
+app.use("/auth", authRoutes);
+app.use("/profile", profileRoutes);
 
+//error 404 handler, after all routers
+app.use(errorMiddleware);
 
+// connection to port after connected to DB
+async function start() {
+    try {
+        connectDB().then(() => {
+            app.listen(PORT, () => {
+                console.log(`Server is running on port ${PORT}`);
+            });
+        });
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+start();
 
